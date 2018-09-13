3e55b8ddd9561feeb0d635da43d0b16d19d2025b
Esmaeel Komijani <EsmaeelKomijani@gmail.com>
2018-09-13 10:17:02 +0430

    made team better

--- noise_project/settings.py
+++ noise_project/settings.py
@@ -126,7 +126,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+COMPRESS_ENABLED = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
@@ -139,8 +139,8 @@ STATICFILES_FINDERS = (
     'compressor.finders.CompressorFinder',
 )
 
-COMPRESS_ROOT = os.path.join(BASE_DIR, 'static')
-COMPRESS_URL = '/compressed/'
+# COMPRESS_ROOT = os.path.join(BASE_DIR, 'static')
+# COMPRESS_URL = '/compressed/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'noise_project/staticfiles/'),
