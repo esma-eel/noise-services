d20fdae289216615853d20ed9dbd38b4eff82d89
Esmaeel Komijani <EsmaeelKomijani@gmail.com>
2018-09-21 19:19:57 +0430

    changed services a little

--- services/models.py
+++ services/models.py
@@ -5,6 +5,7 @@ class Service(models.Model):
     title = models.CharField(max_length=500)
     icon  = models.CharField(max_length=500)
     description = models.TextField()
+    link = models.URLField(blank=True, null=True)
 
 
     def __str__(self):
