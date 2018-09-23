e4d5676bd7127ac58a3af1883f25c57a5c1da15c
Esmaeel Komijani <EsmaeelKomijani@gmail.com>
2018-09-23 21:23:13 +0330

    got back every thing

--- contact/forms.py
+++ contact/forms.py
@@ -1,18 +1,18 @@
 from django import forms
 from .models import Contact
-# from pricing.models import PricePlan
+from pricing.models import PricePlan
 
 
-# choices_plan = [('هیچکدام','هیچکدام'),]
-# for price in PricePlan.objects.all():
-#     choices_plan.append((price.name, price.name))
+choices_plan = [('هیچکدام','هیچکدام'),]
+for price in PricePlan.objects.all():
+    choices_plan.append((price.name, price.name))
 
 
 class ContactForm(forms.ModelForm):
     name = forms.CharField(max_length=500)
     email = forms.EmailField()
     phone = forms.IntegerField()
-    # which_price_plan = forms.ChoiceField(choices=choices_plan, widget=forms.Select())
+    which_price_plan = forms.ChoiceField(choices=choices_plan, widget=forms.Select())
     message = forms.CharField(widget=forms.Textarea()) 
     file = forms.FileField(required=False)
 
@@ -20,7 +20,7 @@ class ContactForm(forms.ModelForm):
     name.widget.attrs.update({'class': 'form-control', 'placeholder': 'نام و نام خانوادگی'})
     email.widget.attrs.update({'class': 'form-control', 'placeholder': 'ایمیل'})
     phone.widget.attrs.update({'class': 'form-control', 'placeholder': 'شماره تماس'})
-    # which_price_plan.widget.attrs.update({'class': 'form-control', 'placeholder': 'کدام تعرفه را انتخاب کردید ؟'})
+    which_price_plan.widget.attrs.update({'class': 'form-control', 'placeholder': 'کدام تعرفه را انتخاب کردید ؟'})
     message.widget.attrs.update({'class': 'form-control', 'placeholder': 'متن پیام یا سفارش'})
     file.widget.attrs.update({'class': 'form-control', 'placeholder': 'ارسال فایل', 'accept':
         ".pdf, .docx, .jpg, .png", 'draggable':"true"})
@@ -32,7 +32,7 @@ class ContactForm(forms.ModelForm):
             'name',
             'email',
             'phone',
-            # 'which_price_plan',
+            'which_price_plan',
             'message',
             'file',
         ]
